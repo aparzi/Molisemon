4e6acf4c08c73a4dfad6e4897539543aba11e3fd
AParziale <angelo@angelo-pc-fisso>
2019-11-30 12:14:14 +0100

    initial commit

---
+++ text/oakspeech.asm
@@ -0,0 +1,57 @@
+_OakSpeechText1::
+	text "Ciao bello!"
+	line "Benvenuto in"
+	cont "** MOLISEMON **"
+
+	para "Mi chiamo Ciccio"
+	line "e sono uno"
+	cont "studiologo!"
+	prompt
+
+_OakSpeechText2A::
+	text "Questa terra"
+	line "è abitata da"
+	cont "persone molto"
+	cont "particolari, che"
+	cont "sono chiamate..."
+	cont "MOLISANI!@@"
+
+_OakSpeechText2B::
+	text $51,"For some people,"
+	line "#MON are"
+	cont "pets. Others use"
+	cont "them for fights."
+
+	para "Myself..."
+
+	para "I study #MON"
+	line "as a profession."
+	prompt
+
+_IntroducePlayerText::
+	text "First, what is"
+	line "your name?"
+	prompt
+
+_IntroduceRivalText::
+	text "This is my grand-"
+	line "son. He's been"
+	cont "your rival since"
+	cont "you were a baby."
+
+	para "...Erm, what is"
+	line "his name again?"
+	prompt
+
+_OakSpeechText3::
+	text "<PLAYER>!"
+
+	para "Your very own"
+	line "#MON legend is"
+	cont "about to unfold!"
+
+	para "A world of dreams"
+	line "and adventures"
+	cont "with #MON"
+	cont "awaits! Let's go!"
+	done
